0f5f64205ffdf0cd4a6bd8048084a1b052e61e85
amanda-adalee <a.arreola95@hotmail.com>
2023-10-05 15:39:31 -0500

    Initial Commit

---
+++ whisk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whisk
+//
+//  Created by Amanda Arreola on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whisk/whiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiskApp.swift
+//  whisk
+//
+//  Created by Amanda Arreola on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct whiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
