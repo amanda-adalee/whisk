4283e48fa26984907bc4edc8602b87c20c692dc5
amanda-adalee <a.arreola95@hotmail.com>
2023-10-12 18:38:21 -0500

    added an image picker

---
+++ whisk/Views/Components/ImagePicker.swift
@@ -0,0 +1,49 @@
+//
+//  ImagePicker.swift
+//  whisk
+//
+//  Created by Amanda Arreola on 10/12/23.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+// Preview
+struct ImagePicker_Previews: PreviewProvider {
+    @State static var image: UIImage? = nil
+    
+    static var previews: some View {
+        ImagePicker(image: $image)
+    }
+}
