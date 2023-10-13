676771ddb2d65e00012c1724e0f58aa524ba5160
amanda-adalee <a.arreola95@hotmail.com>
2023-10-13 18:34:11 -0500

    added heart/favorite icon to recipe card component

--- whisk/Views/Components/RecipeCard.swift
+++ whisk/Views/Components/RecipeCard.swift
@@ -10,43 +10,47 @@ import SwiftUI
 struct RecipeCard: View {
     var recipe: Recipe
     
+    @State private var isFavorite: Bool = false
+    
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: recipe.image)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .overlay(alignment: .top){
-                        Text(recipe.name)
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .shadow(color: .black, radius: 3, x: 0, y: 0)
-                            .frame(minWidth: 160)
-                            .padding()
-                    }
-            } placeholder: {
-                Image(systemName: "photo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40, alignment: .center)
-                    .foregroundColor(.white.opacity(0.7))
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .overlay(alignment: .top) {
-                        Text(recipe.name)
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .shadow(color: .black, radius: 3, x: 0, y: 0)
-                            .frame(minWidth: 350)
-                            .padding()
-                    }
+            VStack {
+                AsyncImage(url: URL(string: recipe.image)) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+
+                } placeholder: {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.white.opacity(0.7))
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
             }
+            .frame(width: 350, height: 220, alignment: .top)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            
+            HStack {
+                Text(recipe.name)
+                    .font(.title3)
+                    .foregroundColor(.primary)
+                            
+                Spacer()  // Pushes the heart icon to the end
+                            
+                Button(action: {
+                    isFavorite.toggle()}) {
+                            Image(systemName: isFavorite ? "heart.fill" : "heart")
+                            .foregroundColor(isFavorite ? .pink : .black)
+                            }
+                        }
+                        .padding([.leading, .trailing, .bottom], 10)
+                    }
         }
-        .frame(width: 350, height: 220, alignment: .top)
-        .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
-        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-        .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
     }
-}
+
 
 #Preview {
     RecipeCard(recipe: Recipe.sampleRecipes[0])
