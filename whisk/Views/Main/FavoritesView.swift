87daca064eb8cb41aeec9dad029ee6bb9d198eaf
amanda-adalee <a.arreola95@hotmail.com>
2023-10-13 21:32:45 -0500

    updated favorites view to only show favorited recipes

--- whisk/Views/Main/FavoritesView.swift
+++ whisk/Views/Main/FavoritesView.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
+// TODO: this list is not updating when a recipe is favorited in the home view, need to fix
 struct FavoritesView: View {
+    @State private var selectedCategory = Category.all
+    @State private var searchTerm = ""
+    
+    var filteredRecipes: [Recipe] {
+        var results = Recipe.sampleRecipes
+        
+        // favorite filter
+        results = results.filter { $0.isFavorite == true }
+        
+        // category filter
+        if selectedCategory != .all {
+            results = results.filter { $0.category.contains(selectedCategory) }
+        }
+        
+        // search term filter
+        if !searchTerm.isEmpty {
+            results = results.filter { $0.name.localizedCaseInsensitiveContains(searchTerm) }
+        }
+        
+        return results
+    }
+    
     var body: some View {
         NavigationView {
-            Text("No recipes have been saved to your favorites yet.")
-                .padding()
-                .navigationTitle("Favorites")
+            ScrollView{
+                
+                CustomCategoryPicker(selectedCategory: $selectedCategory)
+
+                RecipeList(recipes: filteredRecipes)
+            
+            }
+            .navigationTitle("my favorite recipes")
+            .searchable(text: $searchTerm, prompt: "search favorite recipes")
         }
         .navigationViewStyle(.stack)
     }
